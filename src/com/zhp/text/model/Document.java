59b42d1d96eec90ca79c8661fc539260a1a6b3e6
zhp510730568 <510730568@qq.com>
2015-04-01 14:24:05 +0800

    Update Document.java

--- src/com/zhp/text/model/Document.java
+++ src/com/zhp/text/model/Document.java
@@ -7,8 +7,6 @@ import java.util.Map;
  * The Document Object represents the texts that we use for training or 
  * prediction as a bag of words.
  * 
- * @author Vasilis Vryniotis <bbriniotis at datumbox.com>
- * @see <a href="http://blog.datumbox.com/developing-a-naive-bayes-text-classifier-in-java/">http://blog.datumbox.com/developing-a-naive-bayes-text-classifier-in-java/</a>
  */
 public class Document {
     
